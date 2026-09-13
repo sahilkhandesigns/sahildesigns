b83a0b4b15ae86191298c508a1c40420d169377a
Sahil Khan <sahilkhan1925057@gmail.com>
2026-09-13 22:08:36 +0530

    Configure static export for Cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  output: 'export',
+
   allowedDevOrigins: ['192.168.1.8'],
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
